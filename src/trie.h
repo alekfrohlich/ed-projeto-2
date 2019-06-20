d27403af1104f94e6ad99bdb0ffa4e29dbba9177
alek.frohlich <alek.frohlich@gmail.com>
2019-06-20 18:10:21 -0300

    document trie.h

--- src/trie.h
+++ src/trie.h
@@ -17,19 +17,15 @@
 namespace structures {
 
 /*!
-* @brief Calcula o número de componentes conexos na matriz usando vizinhança-4.
-*
-* Utiliza a técnica de rotulação de formas, para tal criando uma matriz temporária
-* do mesmo tamanho da de entrada: este algoritmo utiliza memória na ordem O(w*h).
-*
-* Cada "pixel" é processado em uma fila (FIFO) de tamanho dinâmico, assim como
-* seus vizinhos e assim por diante ate percorrer todos os caminhos do componente.
-*
-* @param E Matriz de entrada.
-* @param height Número de linhas da matriz.
-* @param width Número de colunas da matriz.
-* @return int Número de componentes conexos (formas) encontrados. Zero implica
-* que a matriz é nula/vazia.
+* @brief Árvore de prefixos.
+* 
+* A árvore de prefixos é uma estrutura de dados eficiente no que diz respeito à
+* recuperação de informações. Por isso ela também é conhecida como Trie, de reTRIEval.
+* Usando uma Trie bem organizada, pode-se alcançar complexidade de busca O(M), onde M
+* representa o tamanho máximo de suas chaves. Ao passo que, uma BST balanceada teria tempo
+* proporcional a MlogN, em que N representa o número de chaves na árvore. Essas vantagens
+* todavia não são sem seus custos, uma vez que a Trie perde para a BST em espaço ocupado em 
+* memória.
 */
 struct TrieNode {
 	struct TrieNode* children[ALPHABET_SIZE];
@@ -37,33 +33,35 @@ struct TrieNode {
 };
 
 /*!
-* @brief Cria um novo node.
+* @brief Aloca memória para um novo TrieNode.
 *
-* Faz com que a pos e o len de cada TrieNode sejam zerados e  
-* os seus ponteiros para filhos sejam nulos.
+* A posição e o comprimento de cada TrieNode começam zerados. Além disso, todos
+* as posições do vetor de nodos filhos começam nulas.
 *
-* @return struct TrieNode* Ponteiro para o novo node criado.
+* @return struct TrieNode* Ponteiro para o novo TrieNode criado.
 */
 struct TrieNode*
 initNode();
+
 /*!
-* @brief Adiciona a string word na trie.
+* @brief Adiciona uma chave na árvore de prefixos.
 *
-* @param root Root da trie.
+* @param root Root da árvore.
 * @param word Palavra para inserir.
 * @param pos Posicao no dicionario da palavra a ser inserida.
 * @param len Comprimento da linha no dicionario que possui a palavra a ser inserida.
 */
 void insert(struct TrieNode* root, std::string word, int pos, int len);
+
 /*!
-* @brief Procura uma palavra na trie.
+* @brief Procura por uma palavra na árvore de prefixos.
 *
 * @param word Palavra a ser procurada na trie.
-* @param root Root da trie.
+* @param root Raíz da árvore.
 *
-* @return pair<int,int> Par que indica se a palavra pertence ao dicionário, é apenas um prefixo ou que
-* não pertence ao dicionário. Se a palavra pertence ao dicionário o primeiro 
-* representa a posição da palavra e o segundo o comprimeto da linha. 
+* @return pair<int,int> Par que indica se a palavra pertence ao dicionário, é apenas 
+* um prefixo ou que não pertence ao dicionário. Se a palavra pertencer ao dicionário, a
+* primeira entrada representa a posição da palavra enquanto a segunda, o comprimeto da linha. 
 */
 std::pair<int, int> search(struct TrieNode*, std::string word);
 
