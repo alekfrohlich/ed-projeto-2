90121f405eb0d240cea68ae5840071b4990bc079
Nicolas Goeldner <ngoeldner@hotmail.com>
2019-06-20 16:59:00 -0300

    add comments

--- src/trie.h
+++ src/trie.h
@@ -16,14 +16,67 @@
 
 namespace structures {
 
+/*!
+* @brief Calcula o número de componentes conexos na matriz usando vizinhança-4.
+*
+* Utiliza a técnica de rotulação de formas, para tal criando uma matriz temporária
+* do mesmo tamanho da de entrada: este algoritmo utiliza memória na ordem O(w*h).
+*
+* Cada "pixel" é processado em uma fila (FIFO) de tamanho dinâmico, assim como
+* seus vizinhos e assim por diante ate percorrer todos os caminhos do componente.
+*
+* @param E Matriz de entrada.
+* @param height Número de linhas da matriz.
+* @param width Número de colunas da matriz.
+* @return int Número de componentes conexos (formas) encontrados. Zero implica
+* que a matriz é nula/vazia.
+*/
 struct TrieNode {
 	struct TrieNode* children[ALPHABET_SIZE];
 	int pos, length;
 };
 
+/*!
+* @brief Calcula o número de componentes conexos na matriz usando vizinhança-4.
+*
+* Utiliza a técnica de rotulação de formas, para tal criando uma matriz temporária
+* do mesmo tamanho da de entrada: este algoritmo utiliza memória na ordem O(w*h).
+*
+* Cada "pixel" é processado em uma fila (FIFO) de tamanho dinâmico, assim como
+* seus vizinhos e assim por diante ate percorrer todos os caminhos do componente.
+*
+* @param E Matriz de entrada.
+* @param height Número de linhas da matriz.
+* @param width Número de colunas da matriz.
+* @return int Número de componentes conexos (formas) encontrados. Zero implica
+* que a matriz é nula/vazia.
+*/
 struct TrieNode*
 getNode();
-void insert(struct TrieNode*, std::string, int, int);
+/*!
+* @brief Adiciona a string word na trie.
+*
+* @param root Root da trie.
+* @param word Palavra para inserir.
+* @param pos Posicao no dicionario da palavra a ser inserida.
+* @param len Comprimento da linha no dicionario que possui a palavra a ser inserida.
+*/
+void insert(struct TrieNode* root, std::string word, int pos, int len);
+/*!
+* @brief Calcula o número de componentes conexos na matriz usando vizinhança-4.
+*
+* Utiliza a técnica de rotulação de formas, para tal criando uma matriz temporária
+* do mesmo tamanho da de entrada: este algoritmo utiliza memória na ordem O(w*h).
+*
+* Cada "pixel" é processado em uma fila (FIFO) de tamanho dinâmico, assim como
+* seus vizinhos e assim por diante ate percorrer todos os caminhos do componente.
+*
+* @param E Matriz de entrada.
+* @param height Número de linhas da matriz.
+* @param width Número de colunas da matriz.
+* @return int Número de componentes conexos (formas) encontrados. Zero implica
+* que a matriz é nula/vazia.
+*/
 std::pair<int, int> search(struct TrieNode*, std::string);
 
 // implementacao incluida aqui
