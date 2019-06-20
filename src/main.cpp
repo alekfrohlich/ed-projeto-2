07e2c430648edfc1d90b9350c38cb0ab4e6e5ba7
Nicolas Goeldner <ngoeldner@hotmail.com>
2019-06-20 12:52:40 -0300

    add ifstream main

--- src/main.cpp
+++ src/main.cpp
@@ -9,6 +9,25 @@ int main() {
 
     cin >> filename;  // entrada
 
+    string line;
+    ifstream myfile (filename);
+    if (myfile.is_open()) {
+        while ( getline (myfile,line) ) {
+            string word;
+            int i;
+            for(i=1;i<line.length();i++){
+            word+=line[i];
+                if(line(i) == ']')
+                    break;
+            }
+            word[i] = '\0';
+            length = line.length();
+            trie.insert(word, posProx, length);
+            posProx += line.length();
+        }
+        myfile.close();
+    }
+
     cout << filename << endl;  // esta linha deve ser removida
     
     while (1) {  // leitura das palavras ate' encontrar "0"
